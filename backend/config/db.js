b156eab856acaa83910db3812fd28705923d3c95
Akshat Kumar <akshat1028kumar@gmail.com>
2026-04-05 23:40:34 +0530

    Initial commit

---
+++ backend/config/db.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const mongoURI = process.env.MONGO_URI;
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(mongoURI);
+    console.log('Connected to MongoDB');
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
